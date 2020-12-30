0ecf80f534dcc557838c7cc4127ef37b7394e956
Jasmin Husić <jhsc1988@gmail.com>
2020-12-30 14:16:08 +0100

    add logger lib, update Main, add Vježbe files

--- LAB5-6-7/src/hr/vsite/java/Main.java
+++ LAB5-6-7/src/hr/vsite/java/Main.java
@@ -1,8 +1,24 @@
 package hr.vsite.java;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
+    // stavljam u svaku klasu koju želim logirati
+    // svaka klasa ima svoj logger - kreiranje loggera nije skupo
+
     public static void main(String[] args) {
-	// write your code here
+        logger.info("application start");
+        logger.trace("number of parameters {}", args.length); // {} placeholder za logger
+
+        try {
+            int a = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e){
+            e.printStackTrace();
+            logger.error("Error parsing parameter",e);
+        }
+
     }
 }
